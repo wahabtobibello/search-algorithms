8160ce1b58ab4b0dc87b9f94b93e5bc13a52b4ce
Tobi Bello <bello.tobi@outlook.com>
2018-09-20 01:25:32 +0100

    sreate blueprint for code

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-class Graph:
-    def __init__(self):
-        pass
-
 class Node:
     def __init__(self):
         pass
+
+
+def depth_first_search():
+    pass
+
+
+def depth_limited_search():
+    pass
+
+
+def iterative_deepening_search():
+    pass
+
+
+def bidirectional_search():
+    pass
+
+
+def breadth_first_search():
+    pass
+
+
+def uniform_cost_search():
+    pass
+
+
+root = Node()
